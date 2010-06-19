f8c37539539314f48b7e14f6a4a0005808735fc3
Ian Zamojc <zamtools@zamtools.com>
2010-06-18 22:50:30 -0400

    Updated api version to 1.0

--- jquery.stacktack.js
+++ jquery.stacktack.js
@@ -199,7 +199,7 @@
 
     $.fn.stacktack.defaults = {
         site: 'stackoverflow.com',
-        apiVersion: '0.8',
+        apiVersion: '1.0',
         stylesheet: 'http://app.stacktack.com/stacktack.min.css',
         answerLimit: 0,
         onlyShowAcceptedAnswer: false,
